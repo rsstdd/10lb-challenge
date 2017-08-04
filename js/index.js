cc9676133ba81c8ca29708d938506459e5c9b0de
rsstdd <rosstod@gmail.com>
2017-08-03 17:40:47 -0700

    init commit

---
+++ js/index.js
@@ -0,0 +1,87 @@
+(function() {
+  'use strict';
+  function resizeImage(){
+    //Resize Bottom Right Image
+    const image = document.getElementsByTag("img");
+    const imageParentElement = document.getElementsByClassName('.image');
+    const widthRatio = imageParentElement.offsetWidth / imageRatio.width;
+    const heightRatio = imageParentElement.offsetHeight / imageRatio.height;
+    const ratio = Math.max(widthRatio, heightRatio);
+    
+    image.css({
+      width: Math.ceil(ratio * imageRatio.width) ,
+      height: Math.ceil(ratio * imageRatio.height)
+    });
+  }
+  
+  function outerWidth(el) {
+    const width = el.offsetWidth;
+    const style = getComputedStyle(el);
+
+    width += parseInt(style.marginLeft) + parseInt(style.marginRight);
+    return width;
+  }
+
+  function outerHeight(el, includeMargin){
+    var height = el.offsetHeight;
+
+    if(includeMargin){
+      var style = el.currentStyle || getComputedStyle(el);
+      height += parseInt(style.marginTop) + parseInt(style.marginBottom);
+    } 
+    
+    return height;
+  }
+  
+  function forEachElement(selector, fn) {
+    const elements = document.querySelectorAll(selector);
+    for (let i = 0; i < elements.length; i++) {
+      fn(elements[i], i);
+    }
+  }
+  
+  const equalheight = (container) => {
+    let currentTallest = 0;
+    let currentRowStart = 0;
+    let rowDivs = new Array();
+    let el;
+    let topPosition = 0;
+    // const elements = document.querySelectorAll('div');
+
+    forEachElement(container, function(el){
+      console.log(container);
+      console.log(el);
+      
+      el.style.height = 'auto';
+      topPosition = el.offsetTop;
+
+      if (currentRowStart != topPosition) {
+        for (let currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+          rowDivs[currentDiv].style.height = `${currentTallest}px`;
+        }
+
+        rowDivs.length = 0;
+        currentRowStart = topPosition;
+        currentTallest = outerHeight(el, false);
+        rowDivs.push(el);
+      } else {
+        rowDivs.push(el);
+        currentTallest = (currentTallest < outerHeight(el, false)) ? (outerHeight(el, false)) : (currentTallest);      }
+      
+      for (let currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+        rowDivs[currentDiv].style.height = `${currentTallest}px`;
+      }
+    });
+  }
+    
+  window.onload = () => {
+    equalheight('#resize div');
+  };
+
+
+  window.onresize = () => {
+    equalheight('#resize div');
+    resizeImage('img')
+  };
+
+})();
---
+++ js/test2.js
@@ -0,0 +1,66 @@
+(() => {
+  'use strict';
+
+  function outerHeight(el, includeMargin){
+    let height = el.offsetHeight;
+
+    if (includeMargin) {
+      let style = el.currentStyle || getComputedStyle(el);
+      height += parseInt(style.marginTop) + parseInt(style.marginBottom);
+    } 
+    
+    return height;
+  }
+  
+  function forEachElement(selector, fn) {
+    let elements = document.querySelectorAll(selector);
+
+    for (let i = 0; i < elements.length; i++) {
+      fn(elements[i], i);
+    }
+  }
+
+  const equalheight = (container) => {
+    let currentTallest = 0;
+    let currentRowStart = 0;
+    let rowDivs =[];
+    let el;
+    let topPosition = 0;
+
+    forEachElement(container, (el, i) => {
+      el.style.height = 'auto';
+      topPosition = el.offsetTop;
+
+      if (currentRowStart != topPosition) {
+        console.log(currentRowStart != topPosition);
+        rowDivs.map((div) => {
+          div.style.height = currentTallest + 'px';
+        });
+      
+        rowDivs.length = 0;
+        currentRowStart = topPosition;
+        currentTallest = outerHeight(el, true);
+        rowDivs.push(el);
+      } else {
+        rowDivs.push(el);
+        currentTallest = (currentTallest < outerHeight(el, false)) ? (outerHeight(el, false)) : (currentTallest);     
+      }
+      console.log(rowDivs);
+
+      rowDivs.map((div) => {
+        div.style.height = currentTallest + 'px';
+        console.log('Div: ', div);
+        console.log('height: ', div.style.height);
+      });
+    })
+  }
+    
+  window.onload = () => {
+    equalheight('.resize');
+  };
+
+  window.onresize = () => {
+    equalheight('.resize');
+  };
+
+})();
